ddd211ad74065877639d5508d82fdb9c3f556031
robjlyons <81177492+robjlyons@users.noreply.github.com>
2026-09-03 09:16:20 +0100

    Update MuZero_Simple.py

--- MuZero_Simple.py
+++ MuZero_Simple.py
@@ -326,14 +326,14 @@ class MuZero:
             target_hidden = self.network.representation(next_observations)
             consistency_loss = tf.reduce_mean(tf.square(next_hidden - tf.stop_gradient(target_hidden)))
             loss = value_loss + reward_loss + policy_loss + 0.1 * consistency_loss
-        gradients = tape.gradient(loss, self.network.trainable_variables)
-        gradients, _ = tf.clip_by_global_norm(gradients, self.config.gradient_clip_norm)
-                losses.append(self._train_step(observation, action, reward, next_observation, done, policy_target))
-                observation = next_observation
-                total_reward += reward
-                if done:
-                    break
-            print(f"Episode {episode + 1}: reward={total_reward:.2f}, loss={np.mean(losses):.4f}")
+            gradients = tape.gradient(loss, self.network.trainable_variables)
+            gradients, _ = tf.clip_by_global_norm(gradients, self.config.gradient_clip_norm)
+            losses.append(self._train_step(observation, action, reward, next_observation, done, policy_target))
+            observation = next_observation
+            total_reward += reward
+            if done:
+                break
+        print(f"Episode {episode + 1}: reward={total_reward:.2f}, loss={np.mean(losses):.4f}")
 
     def _train_step(self, observation, action, reward, next_observation, done, policy_target) -> float:
         with tf.GradientTape() as tape:
